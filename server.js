faf2398fa9e106a4125cd1343de8ff14ef7c7e36
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-06-01 14:06:25 +0300

    Basic express http server functionality

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log('hello!');
+const express = require('express');
+const h = require('./hello.js');
+
+
+console.log(h.hello());
+
+const server = express();
+
+server.use((req, res, next) => {
+    console.log("Middleware");
+    req.testVariable = "Puppua";
+    next();
+});
+
+server.get('/', (req, res) => {
+    res.json({
+        hello: h.hello(),
+        testVariable: req.testVariable,
+    })
+});
+
+server.get('*', (req, res) => {
+    res.send('ERROR!!!');
+});
+
+server.listen(3000, () => {
+    console.log('Server started');
+})
